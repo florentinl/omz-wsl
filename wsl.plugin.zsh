ac1be1e5e194bd4dcf7a05555ff435fb40d2b918
Florentin Labelle <florentin.labelle@student-cs.fr>
2022-07-08 19:55:46 +0200

    ✨ initial commit

---
+++ wsl.plugin.zsh
@@ -0,0 +1,10 @@
+function command_not_found_handler {
+  for ext in ${(s:;:)${PATHEXT-".com;.exe;.bat;.cmd;.vbs;.vbe;.js;.jse;.wsf;.wsh;.msc"}}; do
+    if (( $+commands[$1$ext] )); then
+      exec -- "$1$ext" "${@:2}"
+    fi
+  done
+  print -ru2 "command not found: $1"
+  return 127
+}
+
