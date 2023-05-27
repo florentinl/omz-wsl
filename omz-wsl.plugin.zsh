72a00b67233545133cd450b9c8814256b900c2d3
Florentin Labelle <101594032+florentinl@users.noreply.github.com>
2023-05-27 14:43:53 +0200

    Fix explorer.exe unable to open folders and urls

--- omz-wsl.plugin.zsh
+++ omz-wsl.plugin.zsh
@@ -9,3 +9,16 @@ function command_not_found_handler {
   print -ru2 "command not found: $1"
   return 127
 }
+
+# Explorer.exe can open either a file/folder or a URL
+# If it's a URL (i.e. starts with http:// or https://), no need to tamper with it
+# Otherwise, convert the path to Windows format and pass it to explorer.exe
+function explorer {
+  if [[ $1 == http(|s)://* ]]; then
+    explorer.exe "$@"
+  else
+    explorer.exe "$(wslpath -w "$1")"
+  fi
+}
+
+alias exp='explorer.exe'
